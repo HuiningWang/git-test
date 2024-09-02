0661fca1a3b04479f2bd27cd6245e9d0ca40a88c
wanghuining <whuining021021@163.com>
2024-09-02 09:26:28 +0800

    first commit

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Gittest {
     public static void main(String[] args){
         System.out.println("Hello git!");
+        System.out.println("Hello github!");
     }
 }
