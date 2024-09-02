1ba5726f125015224e76aabc159bd124d6b89ca0
wanghuining <whuining021021@163.com>
2024-09-02 10:00:35 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -4,6 +4,6 @@ public class Gittest {
     public static void main(String[] args){
         System.out.println("Hello git!");
         System.out.println("Hello github!");
-        System.out.println("Hello git3!");
+        System.out.println("Hello hot-fix!");
     }
 }
